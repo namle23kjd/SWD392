de21518c4e54f5af98b86f9c08f949119d59a0ca
NET_13_LeXuanPhuongNam <namlxpse173365@fpt.edu.vn>
2025-01-18 01:25:13 +0700

    Auth and Db User

---
+++ src be/Warehouse Management/Data/WareHouseDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Warehouse_Management.Data
+{
+    public class WareHouseDbContext : IdentityDbContext
+    {
+        public WareHouseDbContext(DbContextOptions<WareHouseDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            var adminID = "373e112e - 121b - 4f3f - bb3e - dc30c08b9999";
+            var staffID = "9116bb38-85b1-4345-bf4a-7c0819a8ef3b";
+
+            var role = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = adminID,
+                    ConcurrencyStamp = adminID,
+                    Name = "Admin",
+                    NormalizedName = "Admin".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = staffID,
+                    ConcurrencyStamp = staffID,
+                    Name = "Staff",
+                    NormalizedName = "Staff".ToUpper()
+                }
+            };
+            
+
+            builder.Entity<IdentityRole>().HasData(role);
+        }
+    }
+}
--- src be/Warehouse Management/Program.cs
+++ src be/Warehouse Management/Program.cs
@@ -1,14 +1,63 @@
+
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Warehouse_Management.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<WareHouseDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentityCore<IdentityUser>()
+        .AddRoles<IdentityRole>()
+        .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("WareHouse")
+        .AddEntityFrameworkStores<WareHouseDbContext>()
+        .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 8;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
