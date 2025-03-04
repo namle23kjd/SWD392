5b3843790a73197a683cf65cd24446ad82e8c1f6
Binh <binhltse173315@fpt.edu.vn>
2025-03-04 11:07:16 +0700

    Fix conflict

--- src fe/Streamlining Inventory and Operation/src/pages/Lots.tsx
+++ src fe/Streamlining Inventory and Operation/src/pages/Lots.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import * as React from 'react';
+import { useEffect, useState } from 'react';
 import Breadcrumb from '../components/Breadcrumbs/Breadcrumb';
 import { useNavigate } from 'react-router-dom';
 import {
--- src fe/Streamlining Inventory and Operation/src/services/baseURL.ts
+++ src fe/Streamlining Inventory and Operation/src/services/baseURL.ts
@@ -1,4 +1,2 @@
-// tạm thời làm vầy trc nhé tại header chưa cần token, 
-// tạo .env VITE_REACT_APP_BASE_API_URL=.... 
-console.log('Base API URL:', import.meta.env.VITE_REACT_APP_BASE_API_URL);
-export const baseURL = import.meta.env.VITE_REACT_APP_BASE_API_URL;
+console.log('Base API URL:', import.meta.env.VITE_API_URI);
+export const baseURL = import.meta.env.VITE_API_URI + "/api";
